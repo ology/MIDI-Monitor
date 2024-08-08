ce2d209ab9acab8ecb2f326292fd9ca813ae8183
Gene Boggs <gene@ology.net>
2024-08-08 16:46:58 -0700

    WIP

--- eg/listen.pl
+++ eg/listen.pl
@@ -7,6 +7,8 @@ my $mm = MIDI::Monitor->new(
     port    => 20,
     verbose => 1,
 );
-$mm->list;
+# $mm->list;
 # print join(', ', $mm->event_cmd->@*), "\n";exit;
+$mm->select_port;
+warn __PACKAGE__,' L',__LINE__,' ',$mm->port,"\n";
 # print $mm->monitor;
