07ba41c56cf96de1b9a44bc2d5db392e0ea9dd28
Gene Boggs <gene@ology.net>
2024-08-09 16:12:15 -0700

    Foo

--- eg/listen.pl
+++ eg/listen.pl
@@ -4,11 +4,10 @@ use warnings;
 
 use MIDI::Monitor ();
 my $mm = MIDI::Monitor->new(
-    port    => 20,
+    # port    => 20,
     verbose => 1,
 );
 # $mm->list;
-# print join(', ', $mm->event_cmd->@*), "\n";exit;
 $mm->select_port;
 warn __PACKAGE__,' L',__LINE__,' ',$mm->port,"\n";
 # print $mm->monitor;
